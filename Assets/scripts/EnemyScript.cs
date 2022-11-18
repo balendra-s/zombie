4213b3a03fae37e3bed2798fd10d10f0925a811e
Balendra Sounthararajan <balendrs@stud.ntnu.no>
2022-11-18 16:05:45 +0100

    Added moving zombies

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject stalkerDest;
+    NavMeshAgent stalkerAgent;
+
+    void Start()
+    {
+        stalkerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+
+
+        stalkerAgent.SetDestination(stalkerDest.transform.position);
+
+    }
+}
