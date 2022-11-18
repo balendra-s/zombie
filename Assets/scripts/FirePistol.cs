603d472b683d5bdb05e9a37d8d8b52525b53d696
aleksander <aleksveg@stud.ntnu.no>
2022-11-18 22:52:38 +0100

    added muzzle flash for pistol when shooting, gun sound and gun script

---
+++ Assets/scripts/FirePistol.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePistol : MonoBehaviour
+{
+    public GameObject TheGun;
+    public GameObject MuzzleFlash;
+    public AudioSource GunFire;
+    public bool IsFiring = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (IsFiring == false)
+            {
+                StartCoroutine(FiringPistol());
+            }
+        }
+    }
+
+    IEnumerator FiringPistol()
+    {
+        IsFiring = true;
+        TheGun.GetComponent<Animation>().Play("PistolShot");
+        MuzzleFlash.SetActive(true);
+        MuzzleFlash.GetComponent<Animation>().Play("MuzzleAnimation");
+        
+        GunFire.Play();
+        yield return new WaitForSeconds(0.5f);
+        IsFiring = false; 
+    }
+}
