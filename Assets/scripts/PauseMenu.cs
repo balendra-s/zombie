c21b8e6d0d8d3bb893ef1dcc4ed0b6ce4f27e236
aleksander <aleksveg@stud.ntnu.no>
2022-11-23 19:26:39 +0100

    remoevd pistolshot - play on awake, so there is no gunshot when entering the game (level1).

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -38,11 +39,12 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
-
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitGame()
     {
-
+        Debug.Log("Quitting game...");
+        Application.Quit();
     }
 }
